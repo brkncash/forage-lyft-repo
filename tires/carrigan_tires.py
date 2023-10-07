5ed8f5fe6e667a40a3a3560b131325475f589272
BrokenCashMachine <markiecash@outlook.com>
2023-10-07 02:30:44 +0100

    add carrigan_tires.py

---
+++ tires/carrigan_tires.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+from tires import Tires
+
+
+class CarriganTires(Tires, ABC):
+
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        for tire in self.tire_wear:
+            if tire >= 0.9:
+                return True
+        return False
