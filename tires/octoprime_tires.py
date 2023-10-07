01f48a14c6afcd8e585cea9087a214741ddf7070
BrokenCashMachine <markiecash@outlook.com>
2023-10-07 02:37:47 +0100

    fix typo in OctoprimeTires class name

--- tires/octoprime_tires.py
+++ tires/octoprime_tires.py
@@ -3,7 +3,7 @@ from abc import ABC
 from tires import Tires
 
 
-class Octoprime(Tires, ABC):
+class OctoprimeTires(Tires, ABC):
 
     def __init__(self, tire_wear):
         self.tire_wear = tire_wear
